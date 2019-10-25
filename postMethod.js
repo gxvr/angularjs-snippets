ed3eb8a19418c69e5622d66f168056f1f1b46c76
Geofrey Xvr <geofreyxvr@gmail.com>
2019-10-25 11:29:14 +0300

    Create postMethod.js
    Added post method

---
+++ postMethod.js
@@ -0,0 +1,40 @@
+//This snippet illustrate Angularjs post operation using Shttp service.
+//$http service is used to send or receive data from the remote server using browser's XMLHttpRequest or JSONP.
+
+var app = angular.module('coolApp' []);
+
+app.controller('coolController', function ($scope, $http)) {
+
+    $scope.sendData = function () {
+        $http({
+            method: 'POST',
+            url: 'request-url', //replace with API url
+            data: $scope.data
+
+        }).then(function successCallbak(response) {
+            //success
+            alert(response);
+            console.log('Data sent!')
+
+        }, function errorCallback(response) {
+            //failed
+            //optional
+            alert(response);
+            console.log('Oops! We have errors')
+
+        });
+
+        //////////////////Option 2//////
+
+        //$http.get('request-url', function successCallback(response) {
+        //
+        //  alert(data);
+        //  console.log('Data sent!')
+        //
+        //}, function errorCallback(response) {
+        //
+        //alert(response);
+        //console.log('Oops! We have errors')
+        //});
+    }
+}
